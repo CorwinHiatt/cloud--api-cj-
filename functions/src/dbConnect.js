de7ffe89a9a797b261da1c69a661d39dca1b0c8c
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-10-18 15:48:24 -0400

    cloud

---
+++ functions/src/dbConnect.js
@@ -0,0 +1,14 @@
+import { initializeApp, getApps, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore"
+
+
+import { ServiceAccount } from "../secrets.js";
+
+export function dbConnect(){
+    if(!getApps().length){
+        initializeApp({
+            credential: cert(ServiceAccount)
+        })
+    }
+    return getFirestore()
+}
